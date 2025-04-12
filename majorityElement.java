166bd02167e58817edd00dd71145e7987a0dd1c1
Mayday46 <winstonnlinn@gmail.com>
2025-04-11 22:40:25 -0400

    Added test case, and fix bugs

--- majorityElement.java
+++ majorityElement.java
@@ -1,26 +1,23 @@
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class majorityElement {
 
-
     public int majorityElementSolution(int[] nums) {
-
         int maxValue = Integer.MIN_VALUE;
         Integer maxKey = null;
 
-        Map <Integer, Integer> table = new HashMap<>();
+        Map<Integer, Integer> table = new HashMap<>();
         for (int digit : nums) {
             if (table.containsKey(digit)) {
-                table.put(digit, table.get(digit + 1));
+                table.put(digit, table.get(digit) + 1);  // Fixed this line
             }
             else {
                 table.put(digit, 1);
             }
         }
         
-        for (Map.Entry <Integer, Integer> entry : table.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : table.entrySet()) {
             if (entry.getValue() != null && entry.getValue() > maxValue) {
                 maxValue = entry.getValue();
                 maxKey = entry.getKey();
@@ -28,5 +25,17 @@ public class majorityElement {
         }
 
         return maxKey;
+    }
+
+    public static void main(String[] args) {
+        majorityElement testCase = new majorityElement();
+        int[] nums1 = {3, 2, 3};
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+
+        int result1 = testCase.majorityElementSolution(nums1);
+        int result2 = testCase.majorityElementSolution(nums2);
+
+        System.out.println("Result 1: " + result1); // Output: 3
+        System.out.println("Result 2: " + result2); // Output: 2
     }
 }
