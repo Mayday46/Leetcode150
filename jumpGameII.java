d61880ee52177bea7a48255287cbcbf74673e5e3
Mayday46 <winstonnlinn@gmail.com>
2025-06-09 19:20:21 -0400

    Leetcode 45. Jump Game II + Solution (Optimial Greedy Solution)

---
+++ jumpGameII.java
@@ -0,0 +1,35 @@
+public class jumpGameII {
+    public int jump(int[] nums) {
+        int totalJumps = 0;
+        int destination = nums.length - 1;
+        int coverage = 0, lastJumpIndex = 0;
+
+        if (nums == null || nums.length == 1) return 0;
+
+        for (int i = 0; i< nums.length - 1; i++) {
+            coverage = Math.max(coverage, i + nums[i]); // The longest distance we can reach from the current index.
+
+            if (i == lastJumpIndex) {
+                lastJumpIndex = coverage; // Update the last jump index to the furthest we can reach.
+                totalJumps++; // Increment the jump count.
+
+                if (coverage >= destination) {
+                    return totalJumps; // If we can reach or exceed the destination, return the jump count.
+                }
+            }
+
+        }
+
+
+
+        return totalJumps;
+    }
+
+
+    // test case
+    public static void main(String[] args) {
+        jumpGameII jg = new jumpGameII();
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(jg.jump(nums)); // Output: 2
+    }
+}
