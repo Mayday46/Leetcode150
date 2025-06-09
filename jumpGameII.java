df8113bab583c4a0f7b2e10000a6cb88c3fec7f1
Mayday46 <winstonnlinn@gmail.com>
2025-06-09 19:54:53 -0400

    Refactor variable names for clarity in jump calculation

--- jumpGameII.java
+++ jumpGameII.java
@@ -2,15 +2,15 @@ public class jumpGameII {
     public int jump(int[] nums) {
         int totalJumps = 0;
         int destination = nums.length - 1;
-        int coverage = 0, lastJumpIndex = 0;
+        int coverage = 0, currentRangeEnd = 0;
 
         if (nums == null || nums.length == 1) return 0;
 
         for (int i = 0; i< nums.length - 1; i++) {
             coverage = Math.max(coverage, i + nums[i]); // The longest distance we can reach from the current index.
 
-            if (i == lastJumpIndex) {
-                lastJumpIndex = coverage; // Update the last jump index to the furthest we can reach.
+            if (i == currentRangeEnd) {
+                currentRangeEnd = coverage; // Update the last jump index to the furthest we can reach.
                 totalJumps++; // Increment the jump count.
 
                 if (coverage >= destination) {
